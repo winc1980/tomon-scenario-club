511fcd242cffd0bce2120cb744f27f8bc5a3da41
葛木美奈 <katsuragimina@katsuragiminanoMacBook-Air.local>
2024-11-30 15:05:53 +0900

    newsとactivityを変更しました

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,3 +6,24 @@ export default {
 	},
 	plugins: [],
 }
+
+4
+5
+6
+7
+module.exports = {
+ 
+    plugins: [
+        require('flowbite/plugin')
+    ],
+
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+
+	theme: {
+		extend: {},
+	},
+ 
+}
