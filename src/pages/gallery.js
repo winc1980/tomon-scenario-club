38208e6aeb6d48cc281cbd408cd31ea54bfc4192
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-12-08 19:36:21 +0900

    galleryを自動再生されるように修正

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Autoplay } from 'swiper/modules';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -8,28 +8,25 @@ import 'swiper/css/bundle'
 
 export function gallerySwiper() {
     const swiper = new Swiper('#gallery-swiper', {
-        modules: [Navigation],
+        modules: [Navigation, Autoplay],
         // Optional parameters
         direction: 'horizontal',
         loop: true,
         centeredSlides: true,
         slidesPerView: 2,
         spaceBetween: '16vw',
+        speed: 500,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
     
         // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-    
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    });
-    
-    
-        
+    }); 
 }
 
      
