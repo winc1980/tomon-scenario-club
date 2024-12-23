2a5f7b84c58d97ba91c22a61b9d713adca0046e6
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-12-23 10:50:45 +0900

    ファーストビュースマホUI

--- src/pages/firstview.js
+++ src/pages/firstview.js
@@ -1,33 +1,42 @@
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(ScrollTrigger);
+const matchMedia = window.matchMedia('(max-width: 768px)');
 
-const tl = gsap.timeline({
-  scrollTrigger: {
+if (matchMedia.matches) {
+  setTimeout(function () {
+    document.querySelector('.p-firstview_text h1').style.visibility = 'hidden';
+    document.querySelector('.p-firstview_text p').style.visibility = 'hidden';
+  }, 2000)
+} else {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.scroll-container',
+      start: 'top top',
+      end: '+=100%',
+      scrub: true,
+      pin: true,
+      anticipatePin: 1
+    }
+  });
+
+  tl.to('.p-firstview_text', {
+    autoAlpha: 0,
+    y: -100,
+  }, 0)
+    .to('.p-firstview_background', {
+      autoAlpha: 0,
+      y: -200,
+    }, 0.8);
+
+  ScrollTrigger.create({
     trigger: '.scroll-container',
     start: 'top top',
     end: '+=100%',
-    scrub: true,
-    pin: true,
-    anticipatePin: 1
-  }
-});
-
-tl.to('.p-firstview_text', {
-  autoAlpha: 0,
-  y: -100,
-}, 0)
-  .to('.p-firstview_background', {
-    autoAlpha: 0,
-    y: -200,
-  }, 0.8);
-
-ScrollTrigger.create({
-  trigger: '.scroll-container',
-  start: 'top top',
-  end: '+=100%',
-  onLeave: () => {
-    console.log('animation complete');
-  }
-});
+    onLeave: () => {
+      console.log('animation complete');
+    }
+  });
+}
