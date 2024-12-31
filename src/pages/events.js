a90a048982f8642c6ce69e95c2e1fca930d1731d
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-12-31 12:47:46 +0900

    クライアントサイド、サーバーサイドのエラー解消

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,34 +1,36 @@
-// すべてのテキスト要素を取得
-const texts = document.querySelectorAll(".text");
-// 各要素の出現タイミングを個別に設定（カスタマイズ可能）
-const offsets = [600, 1000, 1400]; // 各テキストのスクロール閾値
+if (typeof document !== "undefined") {
+  // すべてのテキスト要素を取得
+  const texts = document.querySelectorAll(".text");
+  // 各要素の出現タイミングを個別に設定（カスタマイズ可能）
+  const offsets = [600, 1000, 1400]; // 各テキストのスクロール閾値
 
-// スクロールイベントのリスナーを設定
-window.addEventListener("scroll", () => {
-  var scroll = window.scrollY;
-  const windowHeight = window.innerHeight;
-  texts.forEach((text, index) => {
-    if (scroll + windowHeight > text.offsetTop) {
-      text.classList.add("line"); // 下線アニメーションを適用
-    } else {
-      text.classList.remove("line"); // 下線アニメーションを解除
-    }
-  });
-});
-// すべての画像要素を取得
-const images = document.querySelectorAll(".fade-in");
-
-// スクロールイベントのリスナーを設定
-window.addEventListener("scroll", () => {
-  images.forEach((image) => {
-    const rect = image.getBoundingClientRect();
+  // スクロールイベントのリスナーを設定
+  window.addEventListener("scroll", () => {
+    var scroll = window.scrollY;
     const windowHeight = window.innerHeight;
-
-    // 画像が画面内に入ったら表示する
-    if (rect.top < windowHeight - 100) {
-      image.classList.add("visible");
-    } else {
-      image.classList.remove("visible");
-    }
+    texts.forEach((text, index) => {
+      if (scroll + windowHeight > text.offsetTop) {
+        text.classList.add("line"); // 下線アニメーションを適用
+      } else {
+        text.classList.remove("line"); // 下線アニメーションを解除
+      }
+    });
   });
-});
+  // すべての画像要素を取得
+  const images = document.querySelectorAll(".fade-in");
+
+  // スクロールイベントのリスナーを設定
+  window.addEventListener("scroll", () => {
+    images.forEach((image) => {
+      const rect = image.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      // 画像が画面内に入ったら表示する
+      if (rect.top < windowHeight - 100) {
+        image.classList.add("visible");
+      } else {
+        image.classList.remove("visible");
+      }
+    });
+  });
+}
