059171fc70e2c59b0ba7dab9c8c911f7e12f8567
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-12-31 12:51:04 +0900

    クライアントサイド、サーバーサイドのエラー修正

--- src/pages/events2.js
+++ src/pages/events2.js
@@ -1,16 +1,18 @@
-const image = document.getElementById("image"); // ロゴ画像
+if (typeof document !== "undefined") {
+  const image = document.getElementById("image"); // ロゴ画像
 
-// 画像を左端から200px右に動かす
-image.animate(
-  // 途中の状態を表す配列
-  [
-    { transform: "translateX(0)" }, // 開始時の状態（左端）
-    { transform: "translateX(200px)" }, // 終了時の状態（左端から200pxの位置）
-  ],
-  // タイミングに関する設定
-  {
-    fill: "backwards", // 再生前後の状態（再生前、開始時の状態を適用）
-    duration: 1000, // 再生時間（1000ミリ秒）
-    iterations: Infinity, // アニメーションの繰り返し回数（ずっと繰り返す）
-  }
-);
+  // 画像を左端から200px右に動かす
+  image.animate(
+    // 途中の状態を表す配列
+    [
+      { transform: "translateX(0)" }, // 開始時の状態（左端）
+      { transform: "translateX(200px)" }, // 終了時の状態（左端から200pxの位置）
+    ],
+    // タイミングに関する設定
+    {
+      fill: "backwards", // 再生前後の状態（再生前、開始時の状態を適用）
+      duration: 1000, // 再生時間（1000ミリ秒）
+      iterations: Infinity, // アニメーションの繰り返し回数（ずっと繰り返す）
+    }
+  );
+}
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,15 @@
-window.addEventListener('scroll', () => {
-  var scroll = window.scrollY;
-  const windowHeight = window.innerHeight;
-  const hiddenEls = document.querySelectorAll('.is-not-visible');
+if (typeof document !== "undefined") {
+  window.addEventListener('scroll', () => {
+    var scroll = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const hiddenEls = document.querySelectorAll('.is-not-visible');
 
-  hiddenEls.forEach((e) => {
-    const rect = e.getBoundingClientRect();
-    const eTop = scroll + rect.top;
-    if (scroll + windowHeight > eTop) {
-      e.classList.add('is-visible');
-    }
+    hiddenEls.forEach((e) => {
+      const rect = e.getBoundingClientRect();
+      const eTop = scroll + rect.top;
+      if (scroll + windowHeight > eTop) {
+        e.classList.add('is-visible');
+      }
+    })
   })
-})
+}
