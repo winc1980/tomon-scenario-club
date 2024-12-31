8917ce69cb5e1790ae4ef7a8d7fa6df13c932524
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-12-31 15:39:31 +0900

    base修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomonshinaken.github.io',
-  base: '/tomon-scenario-club/',
+  base: '/',
   vite: {
     resolve: {
       alias: {
