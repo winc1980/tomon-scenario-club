3056c33a705e6b6345d00570817b22b2e537d934
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2025-01-23 16:01:54 +0900

    環境変数エラーハンドリング

--- src/pages/fetchWorks.js
+++ src/pages/fetchWorks.js
@@ -1,17 +1,19 @@
 const apiUrl = `${import.meta.env.PUBLIC_MICROCMS_API_URL_3}works`;
 const apiKey = import.meta.env.PUBLIC_MICROCMS_API_KEY_3;
 export async function fetchWorksContents() {
-  try {
-    const response = await fetch(`${apiUrl}?limit=100`, {
-      headers: {
-        'X-API-KEY': apiKey
-      },
-    });
-    const data = await response.json();
-    return data.contents;
-  } catch (error) {
-    console.error(error);
-    return;
+  if (import.meta.env.PUBLIC_MICROCMS_API_URL_3) {
+    try {
+      const response = await fetch(`${apiUrl}?limit=100`, {
+        headers: {
+          'X-API-KEY': apiKey
+        },
+      });
+      const data = await response.json();
+      return data.contents;
+    } catch (error) {
+      console.error(error);
+      return;
+    }
   }
 }
 
